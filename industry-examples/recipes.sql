61bf14f414a40967d3a72aadf226f24031284519
Alex Merced <alex@alexmerced.dev>
2025-11-11 15:37:28 -0500

    Update SQL to use Dremio schema for recipes

--- industry-examples/recipes.sql
+++ industry-examples/recipes.sql
@@ -1,6 +1,7 @@
 -- Create the recipes table with an ARRAY column for ingredients (sample rows)
 -- Note: this uses CREATE TABLE AS SELECT to create a physical table with sample data.
-CREATE TABLE IF NOT EXISTS recipes.recipes AS
+CREATE FOLDER dremio.recipes;
+CREATE TABLE IF NOT EXISTS dremio.recipes.recipes AS
 SELECT 1 AS "id",
        'Mild Salsa' AS "name",
        ARRAY['tomato','onion','cilantro','jalapeno','lime'] AS "ingredients",
@@ -11,8 +12,8 @@ UNION ALL
 SELECT 3, 'Spicy Vindaloo', ARRAY['chicken','chili','ginger','garlic','vinegar','habanero'], CURRENT_TIMESTAMP;
 
 -- Create View where AI is used to classify each recipe as Mild, Medium or Spicy
-CREATE OR REPLACE VIEW recipes.recipes_enhanced AS SELECT id,
+CREATE OR REPLACE VIEW dremiorecipes.recipes_enhanced AS SELECT id,
        name,
        ingredients,
        AI_CLASSIFY('Identify the Spice Level:' || ARRAY_TO_STRING(ingredients, ','), ARRAY [ 'mild', 'medium', 'spicy' ]) AS spice_level
-from   recipes.recipes;
+from   dremio.recipes.recipes;
