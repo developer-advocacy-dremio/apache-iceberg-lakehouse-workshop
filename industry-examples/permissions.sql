62370062b3319704799893743c7cde4cc257d453
Alex Merced <alex@alexmerced.dev>
2025-11-12 09:43:17 -0500

    Add SQL grants for project and namespace permissions

---
+++ industry-examples/permissions.sql
@@ -0,0 +1,8 @@
+-- Give Permissions to project
+GRANT SELECT, VIEW REFLECTION, VIEW JOB HISTORY, USAGE, MONITOR,
+       CREATE TABLE, INSERT, UPDATE, DELETE, DROP, ALTER, EXTERNAL QUERY, ALTER REFLECTION, OPERATE
+ON PROJECT
+TO USER "alphatest2user@alexmerced.com"
+
+-- Give Permissions to Namespace in Catalog
+GRANT USAGE on FOLDER "dremio" to USER "alphatest2user@alexmerced.com"
