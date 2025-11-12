bce8ae3b4a0ab4a0af5b1a073d746f45a7ac3baf
Alex Merced <alex@alexmerced.dev>
2025-11-12 09:44:21 -0500

    Fix SQL syntax for granting permissions

--- industry-examples/permissions.sql
+++ industry-examples/permissions.sql
@@ -2,7 +2,7 @@
 GRANT SELECT, VIEW REFLECTION, VIEW JOB HISTORY, USAGE, MONITOR,
        CREATE TABLE, INSERT, UPDATE, DELETE, DROP, ALTER, EXTERNAL QUERY, ALTER REFLECTION, OPERATE
 ON PROJECT
-TO USER "alphatest2user@alexmerced.com"
+TO USER "alphatest2user@alexmerced.com";
 
 -- Give Permissions to Namespace in Catalog
-GRANT USAGE on FOLDER "dremio" to USER "alphatest2user@alexmerced.com"
+GRANT USAGE on FOLDER "dremio" to USER "alphatest2user@alexmerced.com";
